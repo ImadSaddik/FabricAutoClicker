4f5190c89d25f354cbf1c949918193b989d8c88b
Imad Saddik <simad3647@gmail.com>
2026-03-26 08:58:23 +0100

    feat: handle key press and guard against holding down the key

--- src/main/java/com/imadsaddik/autoclicker/AutoClickerClient.java
+++ src/main/java/com/imadsaddik/autoclicker/AutoClickerClient.java
@@ -2,8 +2,12 @@ package com.imadsaddik.autoclicker;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -14,11 +18,15 @@ public class AutoClickerClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private static KeyMapping toggleKeyMapping;
+    private boolean isModEnabled = false;
+    private boolean wasToggleKeyDown = false;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Initializing the" + MOD_ID + "mod");
+
         registerKeyBind();
+        registerEvents();
     }
 
     private void registerKeyBind() {
@@ -35,5 +43,26 @@ public class AutoClickerClient implements ClientModInitializer {
                 category
             )
         );
+    }
+
+    private void registerEvents() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            handleKeyPress(client);
+        });
+    }
+
+    private void handleKeyPress(Minecraft client) {
+        LocalPlayer player = client.player;
+        if (player == null) return;
+
+        boolean isToggleKeyDown = toggleKeyMapping.isDown();
+
+        if (isToggleKeyDown && !wasToggleKeyDown) {
+            isModEnabled = !isModEnabled;
+            String literal = "Auto clicker: " + (isModEnabled ? "ON" : "OFF");
+            player.displayClientMessage(Component.literal(literal), false);
+        }
+
+        wasToggleKeyDown = isToggleKeyDown;
     }
 }
