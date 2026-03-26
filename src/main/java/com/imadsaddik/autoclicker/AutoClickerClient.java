58b2a8d30873bee9b6d56c907984e3d473765f02
Imad Saddik <simad3647@gmail.com>
2026-03-26 17:56:52 +0100

    feat: implement the auto clicker logic

--- src/main/java/com/imadsaddik/autoclicker/AutoClickerClient.java
+++ src/main/java/com/imadsaddik/autoclicker/AutoClickerClient.java
@@ -6,9 +6,15 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,6 +54,7 @@ public class AutoClickerClient implements ClientModInitializer {
     private void registerEvents() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             handleKeyPress(client);
+            handleAutoClicker(client);
         });
     }
 
@@ -61,8 +68,49 @@ public class AutoClickerClient implements ClientModInitializer {
             isModEnabled = !isModEnabled;
             String literal = "Auto clicker: " + (isModEnabled ? "ON" : "OFF");
             player.displayClientMessage(Component.literal(literal), false);
+
+            if (!isModEnabled) {
+                client.options.keyAttack.setDown(false);
+            }
         }
 
         wasToggleKeyDown = isToggleKeyDown;
+    }
+
+    private void handleAutoClicker(Minecraft client) {
+        ClientLevel level = client.level;
+        LocalPlayer player = client.player;
+
+        if (!isModEnabled || player == null || level == null) {
+            return;
+        }
+
+        if (isLookingAtType(client, HitResult.Type.BLOCK)) {
+            client.options.keyAttack.setDown(true);
+        } else if (isLookingAtType(client, HitResult.Type.MISS)) {
+            client.options.keyAttack.setDown(false);
+        } else if (isLookingAtType(client, HitResult.Type.ENTITY)) {
+            EntityHitResult entityHitResult = (EntityHitResult) client.hitResult;
+            if (entityHitResult == null) {
+                return;
+            }
+
+            Entity targetEntity = entityHitResult.getEntity();
+            if (targetEntity instanceof LivingEntity) {
+                if (player.getAttackStrengthScale(0.0F) >= 1.0F) {
+                    InputConstants.Key attackKey = client.options.keyAttack.getDefaultKey();
+                    KeyMapping.click(attackKey);
+                }
+            }
+        }
+    }
+
+    private boolean isLookingAtType(Minecraft client, HitResult.Type type) {
+        if (client.hitResult != null && client.player != null) {
+            HitResult.Type hitResultType = client.hitResult.getType();
+            return hitResultType == type;
+        }
+
+        return false;
     }
 }
